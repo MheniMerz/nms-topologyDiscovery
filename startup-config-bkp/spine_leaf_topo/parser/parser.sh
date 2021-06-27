5731735d6ad2ea0634f57b9694d076484b241243
mhenimerz <hani.merzouki@gmail.com>
2021-06-27 02:04:45 +0000

    add duplicate mac address scenario

--- startup-config-bkp/spine_leaf_topo/parser/parser.sh
+++ startup-config-bkp/spine_leaf_topo/parser/parser.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 pwd
 # parse step1_show interface
-RESULT=./output.json
+RESULT=../error_scenario/duplicate_mac/output.json
 echo "{" > $RESULT
-for file in ../cmd_output/*/; do
-	src_host=`cut -d/ -f3 <<< $(echo $file)`
+for file in ../error_scenario/duplicate_mac/*/; do
+	src_host=`cut -d/ -f4 <<< $(echo $file)`
 	echo '"'"$src_host"'": {' >> $RESULT;
 	if [[ $src_host == *"server"* ]];
 	then
-		awk -f ./step1_server_interfaces.awk ../cmd_output/$src_host/*ip_addr* >> $RESULT;
-		awk -f ./step5_server_arp.awk ../cmd_output/$src_host/*arp* >> $RESULT;
-		awk -f ./step7_server_iproute.awk ../cmd_output/$src_host/*ip_route* >> $RESULT;
+		awk -f ./step1_server_interfaces.awk ../error_scenario/duplicate_mac/$src_host/*ip_addr* >> $RESULT;
+		awk -f ./step5_server_arp.awk ../error_scenario/duplicate_mac/$src_host/*arp* >> $RESULT;
+		awk -f ./step7_server_iproute.awk ../error_scenario/duplicate_mac/$src_host/*ip_route* >> $RESULT;
 	else
-		awk -f ./step1_interfaces.awk ../cmd_output/$src_host/*interface* >> $RESULT;
-		awk -f ./step1_vlans.awk ../cmd_output/$src_host/*vlan* >> $RESULT;
-		awk -f ./step2_lldp.awk ../cmd_output/$src_host/*lldp* >> $RESULT;
-		awk -f ./step5_arp.awk ../cmd_output/$src_host/*arp* >> $RESULT;
-		awk -f ./step7_iproute.awk ../cmd_output/$src_host/*route* >> $RESULT;
+		awk -f ./step1_interfaces.awk ../error_scenario/duplicate_mac/$src_host/*interface* >> $RESULT;
+		awk -f ./step1_vlans.awk ../error_scenario/duplicate_mac/$src_host/*vlan* >> $RESULT;
+		awk -f ./step2_lldp.awk ../error_scenario/duplicate_mac/$src_host/*lldp* >> $RESULT;
+		awk -f ./step5_arp.awk ../error_scenario/duplicate_mac/$src_host/*arp* >> $RESULT;
+		awk -f ./step7_iproute.awk ../error_scenario/duplicate_mac/$src_host/*route* >> $RESULT;
 	fi
 	echo '},' >> $RESULT
 done;
